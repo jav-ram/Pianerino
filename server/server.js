ab10bd5d6bba3b677f5c24d006a28bb51a19d346
Javier <ram16230@uvg.edu.gt>
2018-04-14 19:52:36 -0600

    primer comit

---
+++ server/server.js
@@ -0,0 +1,38 @@
+// Set up
+var express  = require('express');
+var app      = express();                               // create our app w/ express
+var morgan = require('morgan');             // log requests to the console (express4)
+var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
+var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
+var cors = require('cors');
+
+app.use(morgan('dev'));                                         // log every request to the console
+app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
+app.use(bodyParser.json());                                     // parse application/json
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+app.use(methodOverride());
+app.use(cors());
+
+app.use(function(req, res, next) {
+   res.header("Access-Control-Allow-Origin", "*");
+   res.header('Access-Control-Allow-Methods', 'DELETE, PUT');
+   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+   next();
+});
+
+
+app.post('/a', function(req, res) {
+  let Respuesta = req.body
+  res.json(Respuesta);
+})
+app.get('/a', function(req, res) {
+  let Respuesta = {"r":"El id del usuario es: "}
+  res.send(Respuesta)
+})
+
+
+
+
+// listen (start app with node server.js) ======================================
+app.listen(8080);
+console.log("App listening on port 8080");
