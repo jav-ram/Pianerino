0f44c6e5d460d25f2b9975db323fe4d97ff6f0e7
Rodrigo Alvarado <alv16106@uvg.edu.gt>
2018-04-16 22:50:34 -0600

    Juego en stage super basica (Lee archivos)

--- Piano/nota.js
+++ Piano/nota.js
@@ -1,9 +1,10 @@
 let colores;
 let pos;
 
-function Nota(d, n){
+function Nota(d, n, t){
   this.d = d;
   this.n = n;
+  this.t = t;
   this.x = screen.width - 600;
   pos = [425+12, 400+12, 375+12, 350+12, 325+12, 300+12, 275+12, 250+12];
   colores = [color(255, 102, 102), color(178, 255, 102), color(102, 178, 255), color(255, 255, 102), color(255, 102, 178), color(102, 255, 178), color(192, 192, 192)];
@@ -13,11 +14,17 @@ function Nota(d, n){
     rect(this.x, pos[n], d*20, 20);
   }
 
-  this.pego = function() {
-    var d = dist(this.x, pos[n], 190, pos[n]);
+  this.pego = function(donde, r) {
+    var dis = dist(this.x, pos[n], donde, pos[n]);
+    if (dis< r) {
+      return true;
+    }else {
+      return false;
+    }
   }
 
   this.update = function(){
-    this.x = this.x - 1;
+    this.x = this.x - t;
+
   }
 }
